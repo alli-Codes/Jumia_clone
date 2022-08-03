437a9c530c86877af413811c5d03da53652ed457
alli-Codes <allisonevans081@gmail.com>
2022-08-03 10:36:10 +0100

    Styling the Header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,26 @@
 <template>
-  <div id="header" class="h-16 bg-blue-400">
-    <h1>Hello</h1>
+  <div id="header" class="h-20 bg-white flex items-center justify-evenly px-20">
+    <h1 class=" bg-red-400 w-40">LOGO</h1>
+
+    <div id="search__container" class="flex items-center justify-center flex-auto w-64 px-20 gap-8 ">
+      <input type="text" class="p-3 flex-1 border-solid border border-gray-300 rounded">
+      <button class="bg-orange-400 p-3 w-24 font-semibold text-white rounded">SEARCH</button>
+    </div>
+
+      <div id="user__section" class="w-80 flex justify-evenly">
+        <div id="user__account">
+          <p>Hi, Evans</p>
+        </div>
+
+        <div id="user__support">
+          <p>Help</p>
+        </div>
+
+        <div id="user__cart">
+          <p>Cart</p>
+        </div>
+      </div>
+    
   </div>
 </template>
 
